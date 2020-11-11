ced926a6cdb457994c76d266debd66575a896fa3
falak08 <73591794+falak08@users.noreply.github.com>
2020-11-11 19:29:42 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+var paper,rect1,rect2,rect3,groundSprite;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+paper=createSprite(50,500,20,20,options);
+
+rect1=createSprite(width/2, height-35,200,20);
+rect1.shapeColor="red";
+
+rect2=createSprite(300,620,20,100);
+rect2.shapeColor="red";
+
+rect3=createSprite(500,620,20,100);
+rect3.shapeColor="red";
+
+groundSprite=createSprite(width/2, height-20, width,10);
+	groundSprite.shapeColor=color(255);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	
+	engine = Engine.create();
+	world = engine.world;
+	
+	Engine.run(engine);
+  var options={
+	  isStatic:false,
+	  restitution:0.3,
+	  friction:0.5,
+	  density:1.2
+
+  }
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  keyPressed();
+  drawSprites();
+ 
+}
+
+function keyPressed(){
+	if(keyCode===UP_ARROW){
+		
+	}
+}
+
